c1ec46d2746cd456fed637d5a9856ba2afe440a7
Diyorbek <diyorbek97270@gmail.com>
2024-09-07 21:53:25 +0500

    rasmlardi uchirdim uziz quyvolasiz

--- project/src/Components/ContainerOne.jsx
+++ project/src/Components/ContainerOne.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import imgOne from '../Assets/Img/img1.png'
-import imgTwo from '../Assets/Img/img2.png'
-import imgThree from '../Assets/Img/img3.png'
-import imgFour from '../Assets/Img/img4.png'
-import imgFive from '../Assets/Img/img5.png'
-import imgSix from '../Assets/Img/img6.png'
+
+
 
 const ContainerOne = () => {
     return(
@@ -17,9 +13,9 @@ const ContainerOne = () => {
                 <div style={{display:"flex",justifyContent:"center",textAlign:"center",fontSize:"12px",fontWeight:"300",color:"#E2E2E2"}}>all projects that we have already done , proven can help to use more <br /> comfortable, then can used by client from our business</div>
                 <br /><br />
                 <div style={{display:"flex",justifyContent:"center",gap:"40px",alignItems:"center"}}>
-                    <div><img src={imgOne} style={{width:"200px",height:"200px"}} alt="" /></div>
-                    <div><img src={imgTwo} style={{width:"250px",height:"250px"}} alt="" /></div>
-                    <div><img src={imgThree} style={{width:"200px",height:"200px"}} alt="" /></div>
+                    <div><img src="" style={{width:"200px",height:"200px"}} alt="" /></div>
+                    <div><img src="" style={{width:"250px",height:"250px"}} alt="" /></div>
+                    <div><img src="" style={{width:"200px",height:"200px"}} alt="" /></div>
                 </div>
                 <br />
                 <div style={{display:"flex",justifyContent:"center"}}>
@@ -34,9 +30,9 @@ const ContainerOne = () => {
                 
                 <br /><br />
                 <div style={{display:"flex",justifyContent:"center",gap:"40px",alignItems:"center"}}>
-                    <div><img src={imgFour} style={{width:"250px",height:"225px"}} alt="" /></div>
-                    <div><img src={imgFour} style={{width:"250px",height:"225px"}} alt="" /></div>
-                    <div><img src={imgFour} style={{width:"250px",height:"225px"}} alt="" /></div>
+                    <div><img src="" style={{width:"250px",height:"225px"}} alt="" /></div>
+                    <div><img src="" style={{width:"250px",height:"225px"}} alt="" /></div>
+                    <div><img src="" style={{width:"250px",height:"225px"}} alt="" /></div>
                 </div>
             </div>
             <br /><br /><br />
@@ -51,8 +47,8 @@ const ContainerOne = () => {
 
 
                 <div style={{marginRight:"200px"}}>
-                    <div><img style={{width:"175px",height:"175px",position:"absolute"}} src={imgFive} alt="" /></div>
-                    <div><img style={{width:"200px",height:"200px",position:"absolute"}} src={imgSix} alt="" /></div>
+                    <div><img style={{width:"175px",height:"175px",position:"absolute"}} src="" alt="" /></div>
+                    <div><img style={{width:"200px",height:"200px",position:"absolute"}} src="" alt="" /></div>
                 </div>
             </div>
         </div>
